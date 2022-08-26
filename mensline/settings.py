91c1de46130736f23c93d73f0dbba3bf282a7e03
Sergey Lychagin <lychagin.sergey@mail.ru>
2022-08-26 11:05:29 +0300

    Add Account App

--- mensline/settings.py
+++ mensline/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mensline.wsgi.application'
+
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
