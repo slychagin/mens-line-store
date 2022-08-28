c3e7bcc5be79dfd583150db9969cbc4c28b95615
Sergey Lychagin <lychagin.sergey@mail.ru>
2022-08-28 13:57:51 +0300

    Add Carts App

---
+++ carts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from store.models import Product
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
