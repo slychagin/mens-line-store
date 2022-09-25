2bf986e323d71dae58570d2d4df49bb93398d56e
Sergey Lychagin <lychagin.sergey@mail.ru>
2022-09-25 14:59:54 +0300

    Filter most popular product on home page

--- mensline/views.py
+++ mensline/views.py
@@ -1,21 +1,29 @@
-from django.shortcuts import render
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Count
+from django.shortcuts import render, redirect
+from orders.models import OrderProduct
 from slider.models import Slider
 from store.models import Product, ReviewRating
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('-created_date')
-
-    # Get the reviews
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
-
     # Get photos for slider
     slider_list = Slider.objects.all()
 
+    # Get most popular products
+    filtered_products = (OrderProduct.objects.values('product_id').
+                         annotate(product_count=Count('product_id')).
+                         order_by('-product_count')[:8])
+
+    most_popular_products = [Product.objects.get(id=product['product_id']) for product in filtered_products]
+
+    # Get the reviews
+    reviews = None
+    for product in most_popular_products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+
     context = {
-        'products': products,
+        'most_popular_products': most_popular_products,
         'reviews': reviews,
         'slider_list': slider_list
     }
